842d2e633fbc6df725d2cc9ec8178f0ea3322553
NelfDesign <fabrice.design85@gmail.com>
2018-05-24 21:39:57 +0200

    Jeu de dés

---
+++ app/src/main/java/fr/nelfdesign/diceroll/LancerActivity.java
@@ -0,0 +1,48 @@
+package fr.nelfdesign.diceroll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class LancerActivity extends AppCompatActivity {
+
+    private TextView result;
+    private TextView title;
+    private Button btnRoll;
+    private int max;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lancer);
+
+        result = findViewById(R.id.textResult);
+        title = findViewById(R.id.textTitle);
+        btnRoll = findViewById(R.id.buttonRoll);
+
+        max = getIntent().getIntExtra("max", 0);
+
+        title.setText(max + " Slided Dice");
+        result.setText("");
+
+        btnRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               result.setText(String.valueOf(random(max)));
+            }
+        });
+    }
+
+    //methode random
+    private int random(int max){
+        SecureRandom rand = new SecureRandom();
+        int random = rand.nextInt(max)+ 1;
+        return random;
+    }
+}
---
+++ app/src/main/java/fr/nelfdesign/diceroll/MainActivity.java
@@ -0,0 +1,50 @@
+package fr.nelfdesign.diceroll;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn6,btn20,btn8;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn6 = findViewById(R.id.btn_6);
+        btn8 = findViewById(R.id.btn_8);
+        btn20 = findViewById(R.id.btn_20);
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                page(6);
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                page(8);
+            }
+        });
+
+        btn20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                page(20);
+            }
+        });
+    }
+
+    //methode créant l'intent
+    private void page(int nombre){
+        Intent intent = new Intent(getApplicationContext(), LancerActivity.class);
+        intent.putExtra("max", nombre);
+        startActivity(intent);
+    }
+}
